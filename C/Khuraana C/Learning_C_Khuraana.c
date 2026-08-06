f207e60dac0fd852d604c25b9314423266114240
Haider <haidermehnaaz01@gmail.com>
2026-08-06 20:22:01 +0530

    Initial Commit

---
+++ C/Khuraana C/Learning_C_Khuraana.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+
+int main()
+{
+    {
+        /* Pass or Fail (Question 1) - Start Here */
+        int M1,M2;
+        printf("==================== ");
+        printf("Question 1 ");
+        printf("====================\n");
+        printf("Write Marks in Physics: ");
+        scanf("%d", &M1);
+        printf("Write Marks in Maths: ");
+        scanf("%d", &M2);
+        if(M1>50)
+        {
+           if(M2>50)
+           printf("Result: Pass");
+           else
+           printf("Result: Fail");
+        }
+        else
+        printf("Result: Fail");
+        printf("\n\n");
+        /* Pass or Fail (Question 1) - End Here */
+    }
+
+    {
+        /* Pass or Fail (Question 2) - Start Here */
+        int M1,M2,M3;
+        printf("==================== ");
+        printf("Question 2 ");
+        printf("====================\n");
+        printf("Write Marks in Physics: ");
+        scanf("%d", &M1);
+        printf("Write Marks in Maths: ");
+        scanf("%d", &M2);
+        printf("Write Marks in Biology: ");
+        scanf("%d", &M3);
+        if(M1>50)
+        {
+           if(M2>50)
+           {
+            if(M3>50)
+            printf("Resutl: Pass");
+            else
+            printf("Result: Compartment");
+           }
+           else
+           {
+            if(M3>50)
+            printf("Result: Compartment");
+            else
+            printf("Result: Fail");
+           }
+        }
+        else
+        {
+           if(M2>50)
+           {
+            if(M3>50)
+            printf("Result: Compartment");
+            else
+            printf("Result: Fail");
+           }
+           else
+           printf("Result: Fail");
+        }
+        printf("\n\n");
+        /* Pass or Fail (Question 2) - End Here */
+    }
+
+    {
+        /* Greatest of 3 using nested if (Question 3) - Start Here */
+        int x,y,z;
+        printf("==================== ");
+        printf("Question 3 ");
+        printf("====================\n");
+        printf("Write 3 numbers:\n");
+        scanf("%d%d%d",&x,&y,&z);
+        if(x>y)
+        {
+            if(x>z)
+            printf("Greatest of 3 numbers is %d",x);
+            else
+            printf("Greatest of 3 numbers is %d",z);
+        }
+        else
+        {
+            if(y>z)
+            printf("Greatest of 3 numbers is %d",y);
+            else
+            printf("Greatest of 3 numbers is %d",z);
+        }
+        printf("\n\n");
+        /* Greatest of 3 using nested if (Question 3) - End Here */
+    }
+
+    {
+        /* Greatest of 4 using nested if (Question 4) - Start Here */
+        int a,b,c,d;
+        printf("==================== ");
+        printf("Question 4 ");
+        printf("====================\n");
+        printf("Write 4 numbers:\n");
+        scanf("%d%d%d%d",&a,&b,&c,&d);
+        if(a>b)
+        {
+            if(a>c)
+            {
+                if(a>d)
+                printf("Greatest of 4 numbers is %d",a);
+                else
+                printf("Greatest of 4 numbers is %d",d);
+            }
+            else
+            {
+                if(c>d)
+                printf("Greatest of 4 numbers is %d",c);
+                else
+                printf("Greatest of 4 numbers is %d",d);
+            }
+        }
+        else
+        {
+            if(b>c)
+            {
+                if(b>d)
+                printf("Greatest of 4 numbers is %d",b);
+                else
+                printf("Greatest of 4 numbers is %d",d);
+            }
+            else
+            {
+                if(c>d)
+                printf("Greatest of 4 numbers is %d",c);
+                else
+                printf("Greatest of 4 numbers is %d",d);
+            }
+        }
+        /* Greatest of 4 using nested if (Question 4) - End Here */
+    }
+}
